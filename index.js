3da74326f569ae94618f3342b1ba25b0bb29d260
Jayden Ferrer <jaydenferrer123@gmail.com>
2022-07-28 03:45:40 -0400

    created simple route handlers for home, menu, locations, reservations and order page

--- index.js
+++ index.js
@@ -10,6 +10,30 @@ app.get('/', (req, res) => {
     res.send("Home page");
 })
 
+// home route handler
+app.get('/home', (req, res) => {
+    res.send("Actual home page");
+})
+
+// menu route handler
+app.get("/menu", (req, res) => {
+    res.send("menu page");
+})
+
+// locations route handler
+app.get('/locations', (req, res) => {
+    res.send("location page");
+})
+
+// reservations route handler
+app.get('/reservations', (req, res) => {
+    res.send("reservations page");
+})
+
+// order now route handler
+app.get('/order', (req, res) => {
+    res.send("order now page");
+})
 
 // creating a handler that listens for connections on the specified host and port 
 // in our example, listening for connections to localhost, port 3000 (localhost:3000)
