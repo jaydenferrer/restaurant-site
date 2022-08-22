c41223c8d3b52e6ed61c7d45f02d83d7863767e8
Jayden Ferrer <jaydenferrer123@gmail.com>
2022-08-22 15:23:44 -0400

    Styled home page header and fixed navbar styling

--- public/js/test.js
+++ public/js/test.js
@@ -3,9 +3,9 @@ const navBar = document.querySelector('.main-nav');
 window.addEventListener('scroll', () => {
     if (window.scrollY === 0) {
         console.log("scroll at the top");
-        navBar.style.backgroundColor = 'rgb(255,255,255)';
+        navBar.style.backgroundColor = 'rgb(0, 0, 0)';
     }
     else {
-        navBar.style.backgroundColor = 'rgb(255,255,255, 0.75)';
+        navBar.style.backgroundColor = 'rgb(0, 0, 0, 0.75)';
     }
 })
