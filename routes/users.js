e6d0140e0cfc0cb8547cb43c0856262b1708c566
Jayden Ferrer <jaydenferrer123@gmail.com>
2022-08-24 16:20:12 -0400

    Set up forms for register and login routes

--- routes/users.js
+++ routes/users.js
@@ -8,14 +8,27 @@ const router = express.Router();
 // 1. simply renders a login form
 // 2. will take username and password and authenticate and create session
 router.get('/login', (req, res) => {
-    // res.render('login.ejs')
+    res.render('auth_routes/login.ejs')
 }) 
+
+// route will post the data to db
+// authenticate the user with their username and password, connect them with a session
+router.post('/login', (req, res) => {
+
+})
 
 // to register, create two routes
 // 1. simply renders a register form,
 // 2. will take username, email and hashed passport and create a new user, add to db and create session
 router.get('/register', (req, res) => {
-    res.send("REGISTER PAGE")
+    res.render('auth_routes/register.ejs')
+})
+
+// route will post the data to db
+// given the username, email and password, will create a new user
+// save it to the database
+router.post('/register', (req, res) => {
+    // when data is submitted
 })
 
 router.get('/logout', (req, res) => {
