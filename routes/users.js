7bb75927303f30bf563e128cd06582f807e12424
Jayden Ferrer <jaydenferrer123@gmail.com>
2022-08-24 17:08:46 -0400

    Implemented register route using passport and passport-local-moongoose

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 // contains route handlers for authentication
-
+const User = require('../models/users')
 const express = require('express');
 
 const router = express.Router();
@@ -27,8 +27,32 @@ router.get('/register', (req, res) => {
 // route will post the data to db
 // given the username, email and password, will create a new user
 // save it to the database
-router.post('/register', (req, res) => {
-    // when data is submitted
+router.post('/register', async (req, res) => {
+    // when data is submitted, extract username, email and password
+    console.log(req.body);
+    // destructuring from the req body, contains submitted data
+    const {username, password, email} = req.body;
+    const newUser = new User({username, email})
+    
+    // use passport to add our password hashed to the newUser object (passport-local-mongoose)
+    // taking current user and just adding the hashed password
+    // will autoamtically save into db, so we don't need to call .save()
+    const registeredUser = await User.register(newUser, password);
+    // registeredUser contains the details we need for the user, save that into the database
+    
+    // want to also login the user in after they've registered
+    // login() function available on req that can be used to establish a login session
+    // reason we use req.login in here is mainly when new users register to automatically log the user in if they successfully registered
+    req.login(registeredUser, (err) => {
+        // if an error occurs call error handler
+        if (err) { return next(err) }
+        // if no error occurs, then redirect to restaurants
+        // means that a LOGIN SESSION HAS BEEN ESTABLISHED
+        else {
+            console.log("sucessfully registered and login in user!");
+            return res.redirect('/restaurants');
+        }
+    });
 })
 
 router.get('/logout', (req, res) => {
